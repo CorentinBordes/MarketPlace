17a0a6597b62fb9a4598b56b672730ff69d849f8
BERGIN Emile <bergine@hostname.iut2.upmf-grenoble.fr>
2018-10-18 10:46:22 +0200

    MAJ data

--- model/data/create.sql
+++ model/data/create.sql
@@ -1,1 +1,12 @@
- 
+CREATE TABLE article (
+	ref INTEGER PRIMARY KEY,
+	intitulé TEXT,
+	categorie INTEGER,
+	info TEXT,
+	image TEXT,
+	FOREIGN KEY(categorie) REFERENCES categorie(id)
+);
+CREATE TABLE categorie (
+	id INTEGER PRIMARY KEY,
+	nom TEXT
+);
