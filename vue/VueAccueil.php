d14be1af6fb95f08463d6eaeb426efa685b17b5d
BORDES Corentin <bordesc@hostname.iut2.upmf-grenoble.fr>
2018-10-18 11:04:15 +0200

    MAJ vue

---
+++ vue/VueAccueil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+      <head>
+            <title>date.html</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" type="text/css" href="styleAccueil.css">
+      </head>
+
+      <header>
+
+        <!-- Titre/logo -->
+            <h1>Amazonie</h1>
+
+        <!-- barre de recherche -->
+
+         <!-- boutons d'accueil -->
+
+         <!-- Identifiez-vous -->
+
+         <!-- Panier -->
+
+
+      </header>
+
+      <body>
+
+          <!-- panel gauche avec les catégories -->
+
+          <!-- liste de tout les produits -->
+
+            <!-- image/nom/prix toutes les contraintes -->
+
+
+    </body>
+</html>
