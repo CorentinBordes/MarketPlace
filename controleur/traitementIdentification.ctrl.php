b72b04eb2b9397f89101b2a2daa079e3a9e48ff7
BERGIN Emile <bergine@hostname.iut2.upmf-grenoble.fr>
2018-10-22 08:20:35 +0200

    maj

--- controleur/traitementIdentification.ctrl.php
+++ controleur/traitementIdentification.ctrl.php
@@ -2,5 +2,11 @@
     include_once("../model/DAO.class.php");
     $id=$_POST["id"];
     $password=$_POST["password"];
-    
+    $boolIdentification=$dao->verifIdentification($id,$password);
+    if($boolIdentification){ //personne presente dans la table client
+        $GLOBALS["identificateur"]=;
+        include();
+    }else{                  //personne pas presente dans la table client
+        include();
+    }
  ?>
