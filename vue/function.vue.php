d4fe9f2f7b07bf75b0da9a2a715293baea13f6a5
BERGIN Emile <bergine@hostname.iut2.upmf-grenoble.fr>
2018-10-30 15:22:36 +0100

    maj ajouter au panier

--- vue/function.vue.php
+++ vue/function.vue.php
@@ -7,7 +7,24 @@ function afficherVueArticleSingulier($article,$idClient= NULL): string {
     $result .=  '<p>'.$article->info.'</p>';
     if($article->reduction==0){
         $result .=  '<p>Prix : '.$article->prix.'</p>';
-        formulaireAjouterAuPanier($article->ref,$idClient);
+        if($idClient==NULL){
+            $idClient=0;
+            $result .=  '<form action="../controleur/afficherSIndentifier.ctrl.php" method="post">
+                        Nombre :<br>
+                        <input type="text" name="nombreDArticle" value="1"><br>
+                        <input type="hidden" name="refArticle" value="'.$article->ref.'">
+
+                        <input type="submit" value="Ajouter au panier">
+                        </form>';
+        }else{
+            $result .=  '<form action="../controleur/ajouterAuPanier.php" method="post">
+                        Nombre :<br>
+                        <input type="text" name="nombreDArticle" value="1"><br>
+                        <input type="hidden" name="refArticle" value="'.$article->ref.'">
+                        <input type="hidden" name="idClient" value="'.$idClient.'">
+                        <input type="submit" value="Ajouter au panier">
+                        </form>';
+        }
     }else{
         $prixCal = $article->prix * (1-($article->reduction/100));
         $result .=  '<p>Prix : '.$prixCal.'</p>';
@@ -34,24 +51,7 @@ function afficherVueArticleSingulier($article,$idClient= NULL): string {
 }
 //A REPARER
 function formulaireAjouterAuPanier($RefArticle,$idClient=NULL): string {
-    if($idClient==NULL){
-        $idClient=0;
-        $result .=  '<form action="../controleur/afficherSIndentifier.ctrl.php" method="post">
-                    Nombre :<br>
-                    <input type="text" name="nombreDArticle" value="1"><br>
-                    <input type="hidden" name="refArticle" value="'.$RefArticle.'">
 
-                    <input type="submit" value="Ajouter au panier">
-                    </form>';
-    }else{
-        $result .=  '<form action="../controleur/ajouterAuPanier.php" method="post">
-                    Nombre :<br>
-                    <input type="text" name="nombreDArticle" value="1"><br>
-                    <input type="hidden" name="refArticle" value="'.$RefArticle.'">
-                    <input type="hidden" name="idClient" value="'.$idClient.'">
-                    <input type="submit" value="Ajouter au panier">
-                    </form>';
-    }
     return $result;
 }
 
