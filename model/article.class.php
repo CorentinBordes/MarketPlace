a3ca36fe88237a82e8805fde54c3cc9bb6383468
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2018-12-24 23:00:08 +0100

    Maj jc

--- model/article.class.php
+++ model/article.class.php
@@ -9,27 +9,6 @@
         public $prix;      // le prix
         public $image;     // Nom du fichier image
         public $reduction;
-
-        /*function afficherVueArticleSingulier(): string {
-            $result='<img src="../data/img/'.$this->image.'" alt="image correspondant a l\'objet">';
-            $result .= '<p><b>'.$this->intitulé.'</b></p>';
-            $result .=  '<p>'.$this->info.'</p>';
-            if($this->reduction==0){
-                $result .=  '<p>Prix : '.$this->prix.'</p>';
-            }else{
-                $prixCal = $this->prix * (1-($this->reduction/100));
-                $result .=  '<p>Prix : '.$prixCal.'</p>';
-                $result .=  '<form action="../controleur/ajouterAuPanier.php" method="post">
-                            Nombre :<br>
-                            <input type="text" name="nombreDArticle" value="1"><br>
-                            <input type="hidden" name="refArticle" value="'.$this->ref.'">
-
-                            <input type="submit" value="Ajouter au panier">
-                            </form>
-                '  ;
-            }
-            return $result;
-        }*/
     }
 
 ?>
